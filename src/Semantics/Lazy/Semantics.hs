974f2bde417d469ce71187594e17862c73d0658b
alecsferra <alecsferra@protonmail.com>
2022-11-20 15:44:45 +0100

    generalized semantics

--- src/Semantics/Lazy/Semantics.hs
+++ src/Semantics/Lazy/Semantics.hs
@@ -1,57 +1,15 @@
 module Semantics.Lazy.Semantics (eval) where
 
-import Data.Foldable (Foldable (..))
-import Semantics.Environment (Environment, insert, lookup, withDefault)
-import Syntax.Syntax (Expression (..), FunctionIdentifier (..), VariableIdentifier (..), FunctionDefinition (FunctionDefinition), Program (Program))
-import Util ((.:.))
-import Prelude hiding (lookup)
-import Control.Applicative (Applicative(..))
-import Data.Bool (bool)
-import Semantics.FixPoint (fix)
-
-type FSemantic = Env -> Maybe Integer
-
-data Function = Function [VariableIdentifier] Expression FSemantic
-
-type FEnv = Environment FunctionIdentifier Function
-
-type Env = Environment VariableIdentifier (Maybe Integer)
-
-baseEnv :: Environment k v
-baseEnv =
-    withDefault $ error "Precondition violated the program referenced an undefined identifier"
-
-makeEnv :: Env -> [VariableIdentifier] -> [Maybe Integer] -> Env
-makeEnv base = foldl' (uncurry . insert) base .:. zip
-
-semantics :: Expression -> FEnv -> Env -> Maybe Integer
-semantics e fenv env = run e
-  where
-    semantics' e = semantics e fenv env
-
-    run (Variable id) = lookup env id
-    run (Literal n) = Just n
-    run (Addition l r) = liftA2 (+) (semantics' l) (semantics' r)
-    run (Multiplication l r) = liftA2 (*) (semantics' l) (semantics' r)
-    run (Conditional guard thenBranch elseBranch) =
-      semantics' guard >>= (bool (semantics' thenBranch) (semantics' elseBranch) . (/= 0))
-    run (Application fi args) = do
-      let args' = fmap semantics' args
-      let (Function argNames _ functional) = lookup fenv fi
-      let env' = makeEnv baseEnv argNames args'
-      functional env'
-
-makeBottom :: [FunctionDefinition] -> FEnv
-makeBottom = foldl' (uncurry . insert) baseEnv . fmap makeEmpty
-  where
-    makeEmpty (FunctionDefinition id args body) = (id, Function args body (const Nothing))
-
-step :: FEnv -> FEnv
-step fenv = fmap step fenv
-  where
-    step (Function args body _) = Function args body (semantics body fenv)
+import Semantics.Semantics (EvalStrategy (..))
+import qualified Semantics.Semantics as S (eval)
+import Syntax.Syntax (Program)
 
 eval :: Program -> Integer
-eval (Program definitions mainExpression) = fix (makeBottom definitions) step eval'
-  where
-    eval' f = semantics mainExpression f baseEnv
+eval = S.eval strategy
+
+strategy :: EvalStrategy Maybe
+strategy =
+  EvalStrategy
+    { maybeM = id,
+      arguments = Just
+    }
--- src/Semantics/Strict/Semantics.hs
+++ src/Semantics/Strict/Semantics.hs
@@ -1,58 +1,16 @@
 module Semantics.Strict.Semantics (eval) where
 
-import Control.Applicative (Applicative (liftA2))
-import Control.Monad (forM)
-import Data.Bool (bool)
-import Data.Foldable (Foldable (foldl'))
-import Semantics.Environment (Environment (lookup), insert, withDefault)
-import Semantics.FixPoint (fix)
-import Syntax.Syntax (Expression (..), FunctionDefinition (..), FunctionIdentifier (..), Program (..), VariableIdentifier (..))
-import Util ((.:.))
-import Prelude hiding (lookup)
-
-type FSemantic = Env -> Maybe Integer
-
-data Functional = Functional [VariableIdentifier] Expression FSemantic
-
-type FEnv = Environment FunctionIdentifier Functional
-
-type Env = Environment VariableIdentifier Integer
-
-baseEnv :: Environment k v
-baseEnv =
-    withDefault $ error "Precondition violated the program referenced an undefined identifier"
-
-makeEnv :: Env -> [VariableIdentifier] -> [Integer] -> Env
-makeEnv base = foldl' (uncurry . insert) base .:. zip
-
-semantics :: Expression -> FEnv -> Env -> Maybe Integer
-semantics e fenv env = run e
-  where
-    semantics' e = semantics e fenv env
-
-    run (Variable id) = Just $ lookup env id
-    run (Literal n) = Just n
-    run (Addition l r) = liftA2 (+) (semantics' l) (semantics' r)
-    run (Multiplication l r) = liftA2 (*) (semantics' l) (semantics' r)
-    run (Conditional guard thenBranch elseBranch) =
-      semantics' guard >>= (bool (semantics' thenBranch) (semantics' elseBranch) . (/= 0))
-    run (Application fi args) = do
-      args <- forM args semantics'
-      let (Functional argNames _ functional) = lookup fenv fi
-      let env' = makeEnv baseEnv argNames args
-      functional env'
-
-makeBottom :: [FunctionDefinition] -> FEnv
-makeBottom = foldl' (uncurry . insert) baseEnv . fmap makeEmpty
-  where
-    makeEmpty (FunctionDefinition id args body) = (id, Functional args body (const Nothing))
-
-step :: FEnv -> FEnv
-step fenv = fmap step fenv
-  where
-    step (Functional args body _) = Functional args body (semantics body fenv)
+import Data.Functor.Identity (Identity (Identity, runIdentity), runIdentity)
+import Semantics.Semantics (EvalStrategy (..))
+import qualified Semantics.Semantics as S (eval)
+import Syntax.Syntax (Program)
 
 eval :: Program -> Integer
-eval (Program definitions mainExpression) = fix (makeBottom definitions) step eval'
-  where
-    eval' f = semantics mainExpression f baseEnv
+eval = S.eval strategy
+
+strategy :: EvalStrategy Identity
+strategy =
+  EvalStrategy
+    { maybeM = Just . runIdentity,
+      arguments = fmap (fmap Identity) . sequence
+    }
