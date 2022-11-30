a37e559afd2bc55be8358bf711db80efbb7878e9
alecsferra <alecsferra@protonmail.com>
2022-11-30 12:50:12 +0100

    small refactor

--- src/Semantics/FixPoint.hs
+++ src/Semantics/FixPoint.hs
@@ -1,6 +1,11 @@
-module Semantics.FixPoint (fix) where
+module Semantics.FixPoint (applyFixpoint) where
 
 import Data.Maybe (mapMaybe)
 
-fix :: a -> (a -> a) -> (a -> Maybe b) -> b
-fix bottom f g = head $ mapMaybe g (iterate f bottom)
+applyFixpoint :: a -> (a -> a) -> (a -> Maybe b) -> b
+-- Take the first (f^n bottom) satisfying g
+applyFixpoint bottom f g = head $ mapMaybe g sequence
+  where
+    -- Construct the function application sequence
+    -- f^0 bottom, f^1 bottom, f^2 bottom, ...
+    sequence = iterate f bottom
--- src/Semantics/Semantics.hs
+++ src/Semantics/Semantics.hs
@@ -9,7 +9,7 @@ import Data.Bool (bool)
 import Data.Foldable (Foldable (..))
 import GHC.Base (Applicative (..))
 import Semantics.Environment (Environment, insert, lookup, withDefault)
-import Semantics.FixPoint (fix)
+import Semantics.FixPoint (applyFixpoint)
 import Syntax.Syntax (ConstantDefinition (..), Expression (..), FunctionDefinition (..), FunctionIdentifier (..), Program (..), VariableIdentifier (..))
 import Util ((.:.))
 import Prelude hiding (lookup)
@@ -28,31 +28,40 @@ data EvalStrategy m = EvalStrategy
     pure :: forall a. a -> m a
   }
 
+-- Eval a program using the strategy m, the program must have been already checked
 eval :: forall m. EvalStrategy m -> Program -> Integer
-eval (EvalStrategy toMaybe evalArgs pure) (Program defs consts main) = fix (makeBottom defs) step $ \fenv -> semantics main fenv globalEnv
+eval (EvalStrategy toMaybe evalArgs pure) (Program defs consts main) =
+  -- Find the fix point of F starting from the bottom environment
+  applyFixpoint (makeBottom defs) step $ \fenv -> semantics main fenv globalEnv
   where
+    -- Base empty environment
     baseEnv :: Environment k v
     baseEnv =
       withDefault $ error "Precondition violated the program referenced an undefined identifier"
 
+    -- build the global environment containing all the constants
     globalEnv :: Env m
     globalEnv = uncurry (makeEnv baseEnv) $ unzip $ fmap (second pure . toTuple) consts
       where
         toTuple (ConstantDefinition a b) = (a, b)
 
+    -- The F defined as described in 9.3 pag. 146
     step :: FEnv m -> FEnv m
     step fenv = fmap step' fenv
       where
         step' (Function args body _) = Function args body $ semantics body fenv
 
+    -- Create the bottom FEnv by their definitions
     makeBottom :: [FunctionDefinition] -> FEnv m
     makeBottom = foldl' (uncurry . insert) baseEnv . fmap makeEmpty
       where
         makeEmpty (FunctionDefinition id args body) = (id, Function args body $ const Nothing)
 
+    -- Add the variables in the environment
     makeEnv :: Env m -> [VariableIdentifier] -> [m Integer] -> Env m
     makeEnv base = foldl' (uncurry . insert) base .:. zip
 
+    -- Semantic function of a term as defined in 9.3 pag. 144
     semantics :: Expression -> FEnv m -> Env m -> Maybe Integer
     semantics e fenv env = semantics' e
       where
